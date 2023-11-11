2a50051be574d271ccf09b851368689e9ca40cbf
Kris Lui <68591724+krissolui@users.noreply.github.com>
2023-11-11 18:49:40 -0500

    refactored main.py

--- app/database.py
+++ app/database.py
@@ -2,15 +2,37 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
+import psycopg
+from psycopg.rows import dict_row
 from dotenv import load_dotenv
 
 load_dotenv()
-user = os.environ.get("DB_USER")
-password = os.environ.get("DB_PASSWORD")
+db_user = os.environ.get("DB_USER")
+db_password = os.environ.get("DB_PASSWORD")
 host = os.environ.get("DB_HOST")
 database = os.environ.get("DB_DATABASE")
 
-SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg://{user}:{password}@{host}/{database}"
+# psycopg connection
+while True:
+    try:
+        conn = psycopg.connect(
+            host=host,
+            dbname=database,
+            user=db_user,
+            password=db_password,
+            row_factory=dict_row,
+        )
+
+        cursor = conn.cursor()
+        print("Connected to database")
+        break
+    finally:
+        print("Fail to connection with database")
+
+# SQLAlchemy connection
+SQLALCHEMY_DATABASE_URL = (
+    f"postgresql+psycopg://{db_user}:{db_password}@{host}/{database}"
+)
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- app/main.py
+++ app/main.py
@@ -1,39 +1,14 @@
-import os
 from fastapi import FastAPI
-import psycopg
-from psycopg.rows import dict_row
 from . import models
 from .database import engine
 from dotenv import load_dotenv
 from .routers import auth, post, user
 
-load_dotenv()
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-
-db_user = os.environ.get("DB_USER")
-db_password = os.environ.get("DB_PASSWORD")
-host = os.environ.get("DB_HOST")
-database = os.environ.get("DB_DATABASE")
-
-while True:
-    try:
-        conn = psycopg.connect(
-            host=host,
-            dbname=database,
-            user=db_user,
-            password=db_password,
-            row_factory=dict_row,
-        )
-
-        cursor = conn.cursor()
-        print("Connected to database")
-        break
-    finally:
-        print("Fail to connection with database")
 
 app.include_router(auth.router)
 app.include_router(user.router)
