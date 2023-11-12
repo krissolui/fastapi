c4399e9a103525b7d7ca4cbb2117d1733a3cef41
Kris Lui <68591724+krissolui@users.noreply.github.com>
2023-11-11 19:16:53 -0500

    update settings requirement

--- app/config.py
+++ app/config.py
@@ -3,11 +3,11 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
-    DB_USER: str = ""
-    DB_PASSWORD: str = ""
+    DB_USER: str
+    DB_PASSWORD: str
     DB_HOST: str = "localhost"
-    DB_DATABASE: str = ""
-    OAUTH_SECRET_KEY: str = ""
+    DB_DATABASE: str = "postgres"
+    OAUTH_SECRET_KEY: str
 
 
 config = Settings()
